628687eaf92f014a82edbf43ad249fbf1305c95d
verowangari <verowangari34@gmail.com>
2022-03-07 14:43:14 +0300

    register blueprint with the app in the init file

--- website/__init__.py
+++ website/__init__.py
@@ -7,6 +7,8 @@ def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']='helloworld'
     
+    from .views import views
     
+    app.register_blueprint(views,url_prefix="/")
     
     return app
