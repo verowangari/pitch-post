244e1a979d8b628a713f060434ece8840df85f99
verowangari <verowangari34@gmail.com>
2022-03-07 22:51:06 +0300

    create a models file for creating database

--- website/__init__.py
+++ website/__init__.py
@@ -15,8 +15,16 @@ def create_app():
 
     from .views import views
     from .auth import auth
+   
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+    
+    create_database(app)
 
     return app
+
+def create_database(app):
+    if not path.exists("website/"+ DB_NAME):
+        db.create_all(app=app)
+        print("Created database!")
