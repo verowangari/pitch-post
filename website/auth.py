c2cefeb70c86fb12db4f0f06035cad71d1410c06
verowangari <verowangari34@gmail.com>
2022-03-07 22:26:36 +0300

    collect data from login and signup page

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,21 @@
 from crypt import methods
-from flask import Blueprint,render_template,redirect, url_for 
+from flask import Blueprint,render_template,redirect, request, url_for 
 
 auth=Blueprint("auth",__name__)
 
 @auth.route("/login")
 def login():
-    return render_template("login.html")
+    email=request.form.get("email")
+    password1=request.form.get("password1")
+    return render_template("login.html", methods=['GET','POST'])
 
-@auth.route("/sign-up", methods=['GET,POST'])
+@auth.route("/sign-up", methods=['GET','POST'])
 def sign_up():
+    email=request.form.get("email")
+    username=request.form.get("username")
+    password1=request.form.get("password1")
+    password2=request.form.get("password2")
+    print(username)
     return render_template("signup.html")
 
 @auth.route("/logout")
