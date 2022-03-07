34e94b2ca368b1cd8ebeb4b9781b41a516a0057c
verowangari <verowangari34@gmail.com>
2022-03-07 22:38:20 +0300

    create a database variable

--- website/__init__.py
+++ website/__init__.py
@@ -3,10 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db=SQLAlchemy()
+DB_NAME="database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
+    app.config['SQL-ALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
