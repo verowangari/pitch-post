cace3dfce35dc9d46428af3788f7b1ff134e1315
verowangari <verowangari34@gmail.com>
2022-03-07 14:38:20 +0300

    create views.py for views and auth.py for authentication

--- website/__init__.py
+++ website/__init__.py
@@ -7,13 +7,6 @@ def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']='helloworld'
     
-    @app.route("/")
-    def home():
-        return "<h1> veronica</h1>"
     
-    
-    @app.route("/profile")
-    def profile():
-        return "<h1> my profile page</h1>"
     
     return app
