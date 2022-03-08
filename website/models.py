eb224c456e7147d66a22db31b0a058eb548986e4
verowangari <verowangari34@gmail.com>
2022-03-08 08:05:06 +0300

    create user model and class for holding users information in the database

--- website/models.py
+++ website/models.py
@@ -1,3 +1,8 @@
 from . import db
 from flask_login import UserMixin
 from sql_alchemy.sql import func
+
+class User(db.Model,UserMixin): #Inherits from db model and userMixin
+    id=db.Column(db.Interger,primary_key=True)
+    email=db.Column(db.String(150),unique=True) #prevent users with duplicate emails
+    username=db.Column(db.String(150),unique=True)
