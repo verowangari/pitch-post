d06474f26d7a4b97f387738b84c9628323bbe06a
verowangari <verowangari34@gmail.com>
2022-03-08 11:26:20 +0300

    elifs statement for user authentication

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,5 @@
 from crypt import methods
+from distutils.log import error
 from flask import Blueprint,render_template,redirect, request, url_for,flash 
 from . import db
 from .models import User
@@ -21,8 +22,28 @@ def sign_up():
         password2=request.form.get("password2")
         
         email_exists=User.query.filter_by(email=email).first()
+        username_exists=User.query.filter_by(username=username).first()
         if email_exists:
             flash('The email already exists!', category='error')
+        elif username_exists:
+            flash("The Username already exists",category='error')
+        
+        elif password1!=password2:
+            flash('Password mismatch',category='error')
+        
+        elif len(username) <3:
+            flash('Username too short!',category='error')
+        
+        elif len(password1) <8:
+            flash('Password too short!',category=error)
+            
+        elif len(email)<5:
+            flash('Email is invalid',category='error')
+            
+        else:
+            new_user=User(email=email,username=username,password=password1)
+            
+            
         
     
     # print(username)
