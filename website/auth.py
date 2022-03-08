4cdf7c65bd7c20f5f51cd37e61ce4e2960d51529
verowangari <verowangari34@gmail.com>
2022-03-08 11:03:47 +0300

    write a function for checking if a user exists

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,7 @@
 from crypt import methods
 from flask import Blueprint,render_template,redirect, request, url_for 
+from . import db
+from .models import User
 
 auth=Blueprint("auth",__name__)
 
@@ -11,11 +13,16 @@ def login():
 
 @auth.route("/sign-up", methods=['GET','POST'])
 def sign_up():
+    if request.method =='POST':
     email=request.form.get("email")
     username=request.form.get("username")
     password1=request.form.get("password1")
     password2=request.form.get("password2")
-    print(username)
+    
+    user_exists=User.query.filter_by(email).first()
+    
+    
+    # print(username)
     return render_template("signup.html")
 
 @auth.route("/logout")
