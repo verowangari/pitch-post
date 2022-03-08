004a17610743938fc5ee781564550508c25bdd61
verowangari <verowangari34@gmail.com>
2022-03-08 08:13:57 +0300

    import User model in init file

--- website/models.py
+++ website/models.py
@@ -6,3 +6,5 @@ class User(db.Model,UserMixin): #Inherits from db model and userMixin
     id=db.Column(db.Interger,primary_key=True)
     email=db.Column(db.String(150),unique=True) #prevent users with duplicate emails
     username=db.Column(db.String(150),unique=True)
+    password=db.Column(db.String(150))
+    date_created=db.Column(db.Date(timezone=True),default=func.now()) #if nothing is passed,by default the column will be filled by the current time
