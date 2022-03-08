4aa9f103ccdc1ffce8bba3d2ecec3905412630ae
verowangari <verowangari34@gmail.com>
2022-03-08 10:59:27 +0300

    create a function for creating database

--- website/__init__.py
+++ website/__init__.py
@@ -23,11 +23,15 @@ def create_app():
     from .models import User
     
     create_database(app)
-    
-    login_manager=LoginManager
+   
+    login_manager= LoginManager ()
     login_manager.login_view="auth.login"
     login_manager.init_app(app)
     
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
 
     return app
--- website/models.py
+++ website/models.py
@@ -1,10 +1,10 @@
 from . import db
 from flask_login import UserMixin
-from sql_alchemy.sql import func
+from sqlalchemy.sql import func
 
 class User(db.Model,UserMixin): #Inherits from db model and userMixin
-    id=db.Column(db.Interger,primary_key=True)
+    id=db.Column(db.Integer,primary_key=True)
     email=db.Column(db.String(150),unique=True) #prevent users with duplicate emails
     username=db.Column(db.String(150),unique=True)
     password=db.Column(db.String(150))
-    date_created=db.Column(db.Date(timezone=True),default=func.now()) #if nothing is passed,by default the column will be filled by the current time
+    date_created=db.Column(db.DateTime(timezone=True),default=func.now()) #if nothing is passed,by default the column will be filled by the current time
