8fc40d7ffcd7230ae6994b929e3e31cc3535c3e7
verowangari <verowangari34@gmail.com>
2022-03-09 15:11:01 +0300

    create a class for the Pitches that will inherit from db.Model

--- website/models.py
+++ website/models.py
@@ -8,3 +8,10 @@ class User(db.Model,UserMixin): #Inherits from db model and userMixin
     username=db.Column(db.String(150),unique=True)
     password=db.Column(db.String(150))
     date_created=db.Column(db.DateTime(timezone=True),default=func.now()) #if nothing is passed,by default the column will be filled by the current time
+    pitch=db.relationship('Pitch', backref='user',passive_deletes=True)
+    
+    class Pitch(db.Model):
+        id=db.Column(db.Integer,primary_key=True)
+        text=db.Column(db.Text,nullable=False)
+        date_created=db.Column(db.DateTime(timezone=True),default=func.now())
+        author =db.Column(db.Interger,db.ForeignKey('user.id',ondelete='CASCADE'),nullable=False)
--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from cgitb import text
+from unicodedata import category
+from flask import Blueprint, render_template,request,flash,redirect,url_for
 from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
@@ -13,5 +15,12 @@ def home():
 @views.route("/create-pitch", methods=['GET', 'POST'])
 @login_required
 def create_pitch():
+    if request.method =="POST":
+        text = request.form.get('text')
+        
+        if not text:
+            flash('This cannot be empty',category='error')
+        else:
+            flash('Pitch Created!!',category='success')
     
-     return render_template('create_pitch.html', user=current_user)
+    return render_template('create_pitch.html', user=current_user)
