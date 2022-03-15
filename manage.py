16930305f79979b0f0de065f750c7e511abb2d94
verowangari <verowangari34@gmail.com>
2022-03-15 15:33:32 +0300

    install flask script

---
+++ manage.py
@@ -0,0 +1,21 @@
+# from xml.etree.ElementTree import Comment
+from app import create_app,db
+from flask_script import Manager,Server
+
+
+from app.models import Pitch,User,Comment,Like
+
+# Creating app instance
+app = create_app()
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Like=Like,Comment=Comment,Pitch=Pitch)
+
+if __name__ == '__main__':
+    manager.run()
