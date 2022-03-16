901548222dd573ca7e09ad5b7b2f22c05df4cabd
verowangari <verowangari34@gmail.com>
2022-03-16 13:33:32 +0300

    heroku deploy

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from  flask_migrate import Migrate, MigrateCommand
 from app.models import Pitch,User,Comment,Like
 
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 migrate = Migrate(app,db)
 
 
